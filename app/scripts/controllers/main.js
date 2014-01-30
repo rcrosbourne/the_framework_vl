faec3c2fd56efaf80e601f5f5236576f3dc2349e
Rainaldo Crosbourne <rcrosbourne@gmail.com>
2014-01-29 22:13:03 -0500

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('theFrameworkVlApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
